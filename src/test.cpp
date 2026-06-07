fd7045361cd7c97c53eb11298e0d72a3b78ab1f1
Thyrocytes <reallynewby@icloud.com>
2026-06-07 13:45:05 -0500

    oops

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,7 @@
 #ifndef XBLAZE_BENCHMARKING
 
 #include <Geode/Geode.hpp>
-#include <server.hpp>
+#include "server.hpp"
 
 $on_game(Loaded) {
 	auto start = asp::time::Instant::now();
@@ -12,6 +12,8 @@ $on_game(Loaded) {
 
 	auto elapsed = start.elapsed();
 	geode::log::info("formatResponse: {}", elapsed);
+
+    geode::log::info("{}", xblazeapi::formatResponse("1:6508283:2:ReTraY:3:VGhhbmtzIGZvciBwbGF5aW5nIEdlb21ldHJ5IERhc2g=:4:{levelString}:5:3:6:4993756:8:10:9:10:10:39431612:12:0:13:21:14:4125578:17::43:3:25::18:2:19:7730:42:0:45:20000:15:3:30:0:31:0:28:5 years:29:1 year:35:557117:36:0_733_0_0_0_0_574_716_0_0_352_78_729_0_42_0_833_68_0_347_0_38_240_205_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0_0:37:3:38:1:39:2:46:7729:47:13773:40:0:27:AwMABAYDBw=="));
 }
 
 #endif
